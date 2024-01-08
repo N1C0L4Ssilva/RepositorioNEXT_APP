1ffa7bee80000b60e1b33725d505f5ad263dd0a2
N1C0L4Ssilva <nicolassipva3317@gmail.com>
2024-01-08 16:52:24 -0300

    .

--- pages/CatalogoItens/index.tsx
+++ pages/CatalogoItens/index.tsx
@@ -65,60 +65,10 @@ const CatalogoItens:React.FC=()=>{
 //   const { query } = context;
 //   const marker: string | null = query.MARKER || null;
 
-<<<<<<< HEAD:pages/Catalogo/index.tsx
 //   return {
 //     props: {
 //       marker,
 //     },
 //   };
 // }
-export default Catalogo
-
-/* SCRIPT
-  async ngOnInit(){
-    if(this.valorRecebido){
-      this.Service.GET(`Produto/?${
-        new URLSearchParams({
-          TYPEFIND:"ITEM_BY_MARKER",
-          ITEM_BY_MARKER:this.valorRecebido
-        }).toString()
-      }`).then((v)=>{
-        for(const i in v){
-          if(!this.ITENS.find((Test)=>{return Test.ID==v[i].MARCA})){
-            this.ITENS.push({
-              ID:v[i].MARCA,
-              ITENS:[]
-            })
-          }
-          const LST=this.ITENS.find((Test)=>{return Test.ID==v[i].MARCA})
-          LST.ITENS.push(v[i])
-        }
-      })
-    }
-  }
-*/
-
-/* HTML
-
-<section *ngFor="let Mrk of ITENS" className={style.Result}>
-    <h3 className={style.TituloMarker}>{{Mrk.ID}}</h3>
-    <div className={style.Catalogo}>
-        <app-card-catalogo *ngFor="let I of Mrk.ITENS"
-            Nom={I.NOME}
-            Dec={I.DESCRCAO}
-            Val={I.VALOR}
-            Tag={I.TAGS}
-            ID={I.ID}
-        ></app-card-catalogo>
-    </div>
-</section>
-*/
-=======
-  return {
-    props: {
-      marker,
-    },
-  };
-}
 export default CatalogoItens
->>>>>>> 4aa24606b99d2283d0353d5961d677804db7df08:pages/CatalogoItens/index.tsx
